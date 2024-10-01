934da6746692c34e63c45dc9d4925d99e3901076
ishimwe-diane <ishdiane001@gmail.com>
2024-10-01 15:38:10 +0200

    Add files via upload

---
+++ bb.cs
@@ -0,0 +1,22 @@
+h1{
+background-color:rgb;
+}
+.p{
+font-family:verdana;
+font-size:20px;
+}
+.body{
+	background-image:path;
+background-color:yello;
+}
+div{
+	border:1px solid;
+	border:2px solid;
+	border:2px solid;
+}
+	
+
+.td{
+font-size:10px;
+font:oblique:
+}
